766b4b450c7fa8c067dd47ebe98e7528b8eb177e
Yerbolat <twinspazyl@gmail.com>
2021-10-25 23:56:49 +0600

    part - 2: Call your code from another module

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/qiyal/hello
+
+go 1.17
+
+replace github.com/qiyal/greetings => ../greetings
+
+require github.com/qiyal/greetings v0.0.0-00010101000000-000000000000
---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/qiyal/greetings"
+)
+
+func main() {
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
+}
